614aa7d9c9ffd4b14880b4ebb551338f5a50dfff
Adrian Kuper <adrian.kuper@hotmail.de>
2019-06-16 13:21:14 +0200

    Remove useless comment

--- TechCalendar.Web/webpack.config.js
+++ TechCalendar.Web/webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-// assets.js
 const Assets = require('./assets');
 
 module.exports = {
